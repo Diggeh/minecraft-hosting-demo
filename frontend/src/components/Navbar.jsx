581ea810d04e197b2a94fd9a07f509bb97bd9daf
Jhervis Arevalo <jrarevalo110104@gmail.com>
2026-03-04 04:10:35 +0800

    Improved navbar functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,46 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/logo.png";
 import profileIcon from "../assets/icon_profile.svg";
 
 export default function Navbar() {
-  return (
-    <>
-      <header className="main-header">
-        <div className="header-container">
-          <a href="/" className="logo">
-            <img src={logo} alt="BlockBayan Logo" />
-            <h2>BlockBayan</h2>
-          </a>
-          <nav className="navigation">
-            <a href="/">Home</a>
-            <a href="#plans">Plans</a>
-            <a href="/features">Features</a>
-            <a href="/contactus">Contact us</a>
+  const location = useLocation();
 
-            <div className="auth-group">
-              <div className="line"></div>
-              <div className="profile-btn">
-                <a href="/login">
-                  <img src={profileIcon} alt="Login" />
-                </a>
-                <a href="/login">Login</a>
-              </div>
+  // Check if user is on Login Page
+  const isLoginPage = location.pathname === "/login";
+
+  return (
+    <header className="main-header">
+      <div className="header-container">
+        <a href="/" className="logo">
+          <img src={logo} alt="BlockBayan Logo" />
+          <h2>BlockBayan</h2>
+        </a>
+
+        <nav className="navigation">
+          <a href="/">Home</a>
+
+          {/* Only show these links if NOT on login page */}
+          {!isLoginPage && (
+            <>
+              <a href="#plans">Plans</a>
+              <a href="/features">Features</a>
+              <a href="/contactus">Contact us</a>
+            </>
+          )}
+
+          <div className="auth-group">
+            <div className="line"></div>
+            <div className="profile-btn">
+              <a href="/login">
+                <img src={profileIcon} alt="Login" />
+              </a>
+              <a href="/login">Login</a>
             </div>
-          </nav>
-        </div>
-      </header>
-    </>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 }
