1f431c1bcfc20b67798f52a3e10f5551793733d6
Larusu <ldaneliee33@gmail.com>
2026-03-03 22:40:43 +0800

    refactor navbar and login-page responsiveness for mobile, change the minecraft-image-size-2 size, and move the css to styles/

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.css";
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import profileIcon from "../assets/icon_profile.svg";
 
 export default function Navbar() {
-    return (
-        <>
-            <header className="main-header">
-                <div className="header-container">
-                    <div className="logo">
-                        <img src={logo} alt="BlockBayan Logo" />
-                        <h2>BlockBayan</h2>
-                    </div>
-                    <nav className="navigation">
-                        <a href="/">Home</a>
-                        <a href="/plans">Plans</a>
-                        <a href="/features">Features</a>
-                        <a href="/login">Login</a>
-                        <a href="/contactus">Contact us</a>
-                    </nav>
-                </div>
-            </header>
-        </>
-    )
-};
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-container">
+          <a href="/" className="logo">
+            <img src={logo} alt="BlockBayan Logo" />
+            <h2>BlockBayan</h2>
+          </a>
+          <nav className="navigation">
+            <a href="/">Home</a>
+            <a href="/plans">Plans</a>
+            <a href="/features">Features</a>
+            <a href="/contactus">Contact us</a>
+
+            <div className="auth-group">
+              <div className="line"></div>
+              <div className="profile-btn">
+                <a href="/login">
+                  <img src={profileIcon} alt="Login" />
+                </a>
+                <a href="/login">Login</a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
